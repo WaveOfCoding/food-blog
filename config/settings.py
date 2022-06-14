72eeb972ad1da9478b096d46bd521ce3ed152734
WaveOfCoding <waveformwstcrz@gmail.com>
2022-06-14 16:45:06 +0300

    Add new model contact and templates

--- config/settings.py
+++ config/settings.py
@@ -37,13 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'debug_toolbar',
     'mptt',
-
     'ckeditor',
 
     'blog',
+    'contact'
 ]
 
 MIDDLEWARE = [
