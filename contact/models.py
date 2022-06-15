baeeef2cc33d2aa6b4daea1b4fd7620acde2f33c
WaveOfCoding <waveformwstcrz@gmail.com>
2022-06-15 17:15:13 +0300

    add new templates and modles

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,9 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
 
 class ContactModel(models.Model):
-    """Класс модели обратной связи"""
+    """ Класс модели обратной связи """
     name = models.CharField(max_length=50)
     email = models.EmailField()
     website = models.URLField(blank=True, null=True)
@@ -14,10 +15,37 @@ class ContactModel(models.Model):
 
 
 class ContactLink(models.Model):
-    """Класс модели контакта"""
+    """ Класс модели контакта """
     icon = models.FileField(upload_to="icons/")
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+
+class About(models.Model):
+    """ Класс модели страницы о нас """
+    text = RichTextField()
+    mini_text = RichTextField()
+
+    def get_first_image(self):
+        item = self.about_images.first()
+        return item.image.url
+
+    def get_images(self):
+        return self.about_images.order_by('id')[1:]
+
+
+class ImageAbout(models.Model):
+    """ Класс модели изображений страницы о нас """
+    image = models.ImageField(upload_to="about/")
+    page = models.ForeignKey(About, on_delete=models.CASCADE, related_name="about_images")
+    alt = models.CharField(max_length=100)
+
+
+class Social(models.Model):
+    """ Класс модели соц сетей и страницы о нас """
+    icon = models.FileField(upload_to="icons/")
+    name = models.CharField(max_length=200)
+    link = models.URLField()
 
