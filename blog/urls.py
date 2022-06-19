a1bd2ffa196a54e9a0aa44d91f916c23943b20fe
WaveOfCoding <waveformwstcrz@gmail.com>
2022-06-19 23:15:52 +0300

    fixed some bugs

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('comment/<int:pk>/', views.CreateComment.as_view(), name="create_comment"),
     path('<slug:slug>/<slug:post_slug>/', views.PostDetailView.as_view(), name="post_single"),
     path('<slug:slug>/', views.PostListView.as_view(), name="post_list"),
-    # path('', cache_page(60 * 15)(views.HomeView.as_view()), name="home")
-    path('', views.HomeView.as_view(), name="home"),
+    path('', cache_page(60 * 15)(views.HomeView.as_view()), name="home"),
+    # path('', views.HomeView.as_view(), name="home"),
 ]
